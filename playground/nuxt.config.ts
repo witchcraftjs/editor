640d6924dac1ae92289ead35cdaf98916ad58103
Alan North <alanscodelog@gmail.com>
2025-09-20 20:01:40 -0300

    style: small lints

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,10 +2,14 @@ import pkg from "../package.json" with { type: "json" }
 
 export default defineNuxtConfig({
 	modules: [
+
 		"@witchcraft/ui/nuxt",
-		// the below also works, just remember to run the update-dep script and uncomment ../src/module above before attempting to use the file: linked module
-		"@witchcraft/editor/nuxt"
+		// this won't work for local dev because both the app and the module will be using the ui library
+		// and it uses symbols for value injection and because they're using the library
+		// from different node_modules, it will fail
 		// "../src/module"
+		// this works, just remember to run the update-dep script and uncomment ../src/module above before attempting to use the file: linked module
+		"@witchcraft/editor/nuxt"
 	],
 	devtools: { enabled: true },
 	app: {
